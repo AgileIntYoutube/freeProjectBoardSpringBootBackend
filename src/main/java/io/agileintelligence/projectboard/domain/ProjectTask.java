a0a057b6e2873a0014933ca7030e69431f9bd434
AgileIntelligence <io.agileintelligence@gmail.com>
2018-10-17 14:22:16 -0400

    first commit

---
+++ src/main/java/io/agileintelligence/projectboard/domain/ProjectTask.java
@@ -0,0 +1,68 @@
+package io.agileintelligence.projectboard.domain;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.validation.constraints.NotBlank;
+import java.util.Date;
+
+@Entity
+public class ProjectTask {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank(message = "Please include a project summary")
+    private String summary;
+    private String acceptanceCriteria;
+    private String status;
+    @JsonFormat(pattern = "yyyy-mm-dd")
+    private Date dueDate;
+
+    public ProjectTask() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getSummary() {
+        return summary;
+    }
+
+    public void setSummary(String summary) {
+        this.summary = summary;
+    }
+
+    public String getAcceptanceCriteria() {
+        return acceptanceCriteria;
+    }
+
+    public void setAcceptanceCriteria(String acceptanceCriteria) {
+        this.acceptanceCriteria = acceptanceCriteria;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public Date getDueDate() {
+        return dueDate;
+    }
+
+    public void setDueDate(Date dueDate) {
+        this.dueDate = dueDate;
+    }
+}
---
+++ src/main/java/io/agileintelligence/projectboard/web/ProjectTaskController.java
@@ -0,0 +1,13 @@
+package io.agileintelligence.projectboard.web;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/board")
+@CrossOrigin
+public class ProjectTaskController {
+
+
+}
