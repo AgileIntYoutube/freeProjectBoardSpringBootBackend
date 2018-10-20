9c3f66e5db5c7060680eabc6002217c4754523b7
AgileIntYoutube <io.agileintelligence@gmail.com>
2018-10-20 07:44:16 -0400

    Create Operation

--- src/main/java/io/agileintelligence/projectboard/service/ProjectTaskService.java
+++ src/main/java/io/agileintelligence/projectboard/service/ProjectTaskService.java
@@ -1,5 +1,6 @@
 package io.agileintelligence.projectboard.service;
 
+import io.agileintelligence.projectboard.domain.ProjectTask;
 import io.agileintelligence.projectboard.repository.ProjectTaskRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -9,4 +10,9 @@ public class ProjectTaskService {
 
     @Autowired
     private ProjectTaskRepository projectTaskRepository;
+
+
+    public ProjectTask saveORupdateProjectTask(ProjectTask projectTask){
+        return projectTaskRepository.save(projectTask);
+    }
 }
--- src/main/java/io/agileintelligence/projectboard/web/ProjectTaskController.java
+++ src/main/java/io/agileintelligence/projectboard/web/ProjectTaskController.java
@@ -1,13 +1,45 @@
 package io.agileintelligence.projectboard.web;
 
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import io.agileintelligence.projectboard.domain.ProjectTask;
+import io.agileintelligence.projectboard.service.ProjectTaskService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/api/board")
 @CrossOrigin
 public class ProjectTaskController {
+
+    @Autowired
+    private ProjectTaskService projectTaskService;
+
+    @PostMapping("")
+    public ResponseEntity<?> addPTToBoard(@Valid @RequestBody ProjectTask projectTask , BindingResult result){
+
+        // Check if there are errors
+        if(result.hasErrors()){
+            Map<String, String> errorMap = new HashMap<>();
+
+            for(FieldError error: result.getFieldErrors()){
+                errorMap.put(error.getField(), error.getDefaultMessage());
+            }
+
+            return  new ResponseEntity<Map<String, String>>(errorMap, HttpStatus.BAD_REQUEST);
+        }
+
+        //Save the new object
+        ProjectTask newTask = projectTaskService.saveORupdateProjectTask(projectTask);
+        //Return the object that was created
+        return new ResponseEntity<ProjectTask>(newTask, HttpStatus.CREATED);
+    }
 
 
 }
